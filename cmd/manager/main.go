8968dd5afe9d6ed43826ff7391d5d51642ff1e24
Ivan Valdes (@ivanvc) <iv@a.ki>
2022-10-14 17:41:57 -0700

    Add web service

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -33,6 +33,7 @@ import (
 
 	dispatcherv1alpha1 "github.com/ivanvc/dispatcher/pkg/api/v1alpha1"
 	"github.com/ivanvc/dispatcher/pkg/controllers"
+	"github.com/ivanvc/dispatcher/pkg/http"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -52,8 +53,13 @@ func main() {
 	var metricsAddr string
 	var enableLeaderElection bool
 	var probeAddr string
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	var webServerAddr string
+	flag.StringVar(&webServerAddr, "web-server-bind-address", ":8000",
+		"The address the web server endpoint binds to.")
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080",
+		"The address the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081",
+		"The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
@@ -85,7 +91,7 @@ func main() {
 		// LeaderElectionReleaseOnCancel: true,
 	})
 	if err != nil {
-		setupLog.Error(err, "unable to start manager")
+		setupLog.Error(err, "Unable to start manager")
 		os.Exit(1)
 	}
 
@@ -93,23 +99,28 @@ func main() {
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "JobExecution")
+		setupLog.Error(err, "Unable to create controller", "controller", "JobExecution")
 		os.Exit(1)
 	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
-		setupLog.Error(err, "unable to set up health check")
+		setupLog.Error(err, "Unable to set up health check")
 		os.Exit(1)
 	}
 	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
-		setupLog.Error(err, "unable to set up ready check")
+		setupLog.Error(err, "Unable to set up ready check")
 		os.Exit(1)
 	}
 
-	setupLog.Info("starting manager")
+	if err := mgr.Add(http.NewServer(webServerAddr, mgr.GetClient())); err != nil {
+		setupLog.Error(err, "Error running Web Server")
+		os.Exit(1)
+	}
+
+	setupLog.Info("Starting manager")
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
-		setupLog.Error(err, "problem running manager")
+		setupLog.Error(err, "Error running manager")
 		os.Exit(1)
 	}
 }
