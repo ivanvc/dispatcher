093f74477f7184afc8a06c13f6cbf1c271fec12c
Ivan Valdes (@ivanvc) <iv@a.ki>
2022-10-17 22:05:54 -0700

    Update handler to create PVCInstances too

--- pkg/http/execute_job_handler.go
+++ pkg/http/execute_job_handler.go
@@ -7,9 +7,11 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/ivanvc/dispatcher/pkg/api/v1alpha1"
+	"github.com/google/uuid"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	ctrllog "sigs.k8s.io/controller-runtime/pkg/log"
+
+	"github.com/ivanvc/dispatcher/pkg/api/v1alpha1"
 )
 
 const defaultNamespace = "default"
@@ -31,12 +33,16 @@ func (e *executeJobHandler) handle(w http.ResponseWriter, req *http.Request) {
 	ctx := req.Context()
 	log := ctrllog.FromContext(ctx)
 
-	jobExecution, err := createJobExecution(req)
+	uuid := uuid.New().String()
+
+	name, ns, err := getNameAndNamespace(req.URL.Path)
 	if err != nil {
-		log.Error(err, "Error creating JobExecution")
+		log.Error(err, "Error getting name and namespace")
 		w.WriteHeader(http.StatusNotAcceptable)
 		return
 	}
+
+	jobExecution, pvcInstance := createPayloads(name, ns, uuid, req.Body)
 
 	if err := e.Create(ctx, jobExecution); err != nil {
 		log.Error(err, "Error creating JobExecution")
@@ -44,7 +50,20 @@ func (e *executeJobHandler) handle(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	if err := e.Create(ctx, pvcInstance); err != nil {
+		log.Error(err, "Error creating PVCInstance")
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+
 	w.WriteHeader(http.StatusCreated)
+}
+
+func createPayloads(name, ns, uuid string, body io.ReadCloser) (*v1alpha1.JobExecution, *v1alpha1.PersistentVolumeClaimInstance) {
+	ts := metav1.Now()
+	jobExecution := createJobExecution(name, ns, uuid, ts, body)
+	pvcInstance := createPVCInstance(name, ns, uuid, ts)
+	return jobExecution, pvcInstance
 }
 
 func getNameAndNamespace(path string) (name, namespace string, err error) {
@@ -60,26 +79,37 @@ func getNameAndNamespace(path string) (name, namespace string, err error) {
 	return
 }
 
-func createJobExecution(req *http.Request) (*v1alpha1.JobExecution, error) {
-	name, namespace, err := getNameAndNamespace(req.URL.Path)
-	if err != nil {
-		return nil, err
-	}
-
+func createJobExecution(name, ns, uuid string, timestamp metav1.Time, body io.ReadCloser) *v1alpha1.JobExecution {
 	var b bytes.Buffer
-	if req.Body != nil {
-		defer req.Body.Close()
-		io.Copy(&b, req.Body)
+	if body != nil {
+		defer body.Close()
+		io.Copy(&b, body)
 	}
 
 	return &v1alpha1.JobExecution{
 		ObjectMeta: metav1.ObjectMeta{
 			GenerateName: name + "-",
-			Namespace:    namespace,
+			Namespace:    ns,
 		},
 		Spec: v1alpha1.JobExecutionSpec{
 			JobTemplateName: name,
 			Payload:         b.String(),
+			UUID:            uuid,
+			Timestamp:       timestamp,
 		},
-	}, nil
+	}
+}
+
+func createPVCInstance(name, ns, uuid string, timestamp metav1.Time) *v1alpha1.PersistentVolumeClaimInstance {
+	return &v1alpha1.PersistentVolumeClaimInstance{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: name + "-",
+			Namespace:    ns,
+		},
+		Spec: v1alpha1.PersistentVolumeClaimInstanceSpec{
+			JobTemplateName: name,
+			UUID:            uuid,
+			Timestamp:       timestamp,
+		},
+	}
 }
