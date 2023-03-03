484d6f7eaac227412fe8f3cc8a349c0b11ce8227
Ivan Valdes <iv@a.ki>
2023-03-03 14:24:20 -0800

    Inject sprig funtions in job template

--- pkg/template/job.go
+++ pkg/template/job.go
@@ -7,6 +7,7 @@ import (
 
 	batchv1 "k8s.io/api/batch/v1"
 
+	"github.com/Masterminds/sprig/v3"
 	"github.com/ivanvc/dispatcher/pkg/api/v1alpha1"
 )
 
@@ -15,7 +16,7 @@ func BuildJob(jobTemplateSpec *batchv1.JobTemplateSpec, jobExecution *v1alpha1.J
 	if err != nil {
 		return nil, err
 	}
-	t := template.Must(template.New("job").Parse(string(tpl)))
+	t := template.Must(template.New("job").Funcs(sprig.TxtFuncMap()).Parse(string(tpl)))
 
 	r, w := io.Pipe()
 	go func() {
