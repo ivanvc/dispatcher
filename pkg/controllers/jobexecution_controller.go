b4a3c5007f1ad282a454d6d8f26aaaad6607f21e
Ivan Valdes <iv@a.ki>
2023-07-28 16:51:38 -0700

    fix: allow the controller manager to write events

--- pkg/controllers/jobexecution_controller.go
+++ pkg/controllers/jobexecution_controller.go
@@ -80,6 +80,7 @@ type JobExecutionReconciler struct {
 //+kubebuilder:rbac:groups=dispatcher.ivan.vc,resources=jobexecutions/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=dispatcher.ivan.vc,resources=jobexecutions/finalizers,verbs=update
 //+kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=events,verbs=create;patch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
